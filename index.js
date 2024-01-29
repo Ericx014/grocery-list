edc2803ed393df59bba30454bda4126b89798ff1
Eric Bong <ericbong6868@gmail.com>
2024-01-29 19:02:55 +0800

    updated

---
+++ index.js
@@ -0,0 +1,104 @@
+require("dotenv").config();
+const express = require("express");
+const app = express();
+const cors = require("cors")
+const Item = require("./models/item");
+
+app.use(express.json());
+app.use(
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
+
+app.get("/api/items", async (request, response) => {
+  try {
+    const items = await Item.find({});
+    response.json(items);
+  } catch {
+    console.error("Error", error);
+    response.status(500).json({error: "Internal server error"});
+  }
+});
+
+app.get("/api/items/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const specificItem = await Item.findById(id);
+
+    if (specificItem) {
+      response.json(specificItem);
+      console.log(`Displayed item with id of ${id}`);
+    } else {
+      response.status(404).json({error: `No item with id of ${id} found`});
+    }
+  } catch (error) {
+    console.error("Error", error);
+    response.status(500).json({error: "Internal server error"});
+  }
+});
+
+app.post("/api/items", async (request, response) => {
+  try {
+    const {title, note} = request.body;
+    !title ? response.end(204).json({error: "Title missing"}) : {};
+    const newItem = new Item({title, note});
+    const savedItem = await newItem.save();
+    response.status(201).json(savedItem);
+  } catch {
+    console.error("Error:", error);
+    response.status(500).json({error: "Internal server error"});
+  }
+});
+
+app.delete("/api/items/:id", async (request, response) => {
+  try {
+    const id = request.params.id;
+    const deletedItem = await Item.findByIdAndDelete(id);
+
+    if (deletedItem) {
+      response.status(204).end;
+      console.log(`Deleted item with the id of ${id}`);
+    } else {
+      response.status(404).json({error: `No item with id of ${id} found`});
+    }
+  } catch {
+    console.error("Error:", error);
+    response.status(500).json({error: "Internal server error"});
+  }
+});
+
+app.put("/api/items/:id", async (request, response) => {
+	try {
+		const id = request.params.id
+		const {title, note} = request.body
+    const updatedItem = await Item.findByIdAndUpdate(
+      id,
+      {title, note}
+    );
+
+		if (!title) {
+      return response.status(400).json({error: "Title missing"});
+    }
+
+		if (updatedItem) {
+			response.json(updatedItem)
+			console.log(`Updated item with the id of ${id}`);
+		} else {
+      response.status(404).json({error: `No item with id of ${id} found`});
+    }
+
+	} catch {
+		console.error("Error:", error);
+    response.status(500).json({error: "Internal server error"});
+	}
+})
+
+const unknownEndpoint = (request, response) => {
+	response.status(404).send({error: "unknown endpoint"})
+}
+app.use(unknownEndpoint)
+
+const PORT = process.env.PORT;
+app.listen(process.env.PORT);
+console.log(`Server running on port ${PORT}`);
---
+++ models/item.js
@@ -0,0 +1,30 @@
+require("dotenv").config();
+
+const mongoose = require("mongoose");
+mongoose.set("strictQuery", false);
+
+const url = process.env.MONGODB_URI
+console.log("Connecting to", url);
+
+mongoose.connect(url)
+	.then(result => {
+		console.log("Connected to MongoDB")
+	})
+	.catch((error) => {
+		console.log("Error connecting to MongoDB:", error.message)
+	})
+
+const itemSchema = new mongoose.Schema({
+  title: String,
+  note: String,
+});
+
+itemSchema.set("toJSON", {
+  transform: (document, returnedObject) => {
+    returnedObject.id = returnedObject._id.toString();
+    delete returnedObject._id;
+    delete returnedObject.__v;
+  },
+});
+
+module.exports = mongoose.model("Item", itemSchema)
