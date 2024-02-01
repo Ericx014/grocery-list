87c4f0fe2465dfebc637900fbedbf7087ccce9f3
Eric Bong <ericbong6868@gmail.com>
2024-02-01 19:43:25 +0800

    updated

--- controllers/users.js
+++ controllers/users.js
@@ -4,9 +4,20 @@ const User = require("../models/user")
 
 usersRouter.get("/", async (request, response) => {
   const users = await User.find({}).populate("items");
-
   response.json(users);
 });
+
+usersRouter.get("/:id", async (request, response) => {
+	const id = request.params.id;
+	const user = await User.findById(id).populate("items");
+
+	if (user) {
+    response.json(user);
+    console.log(`Displayed item with id of ${id}`);
+  } else {
+    response.status(404).json({error: `No item with id of ${id} found`});
+  }
+})
 
 usersRouter.post("/", async (request, response) => {
 	const {username, name, password} = request.body
